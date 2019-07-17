1489ba23656bcf3a7b5b385184cc3e8ca018d3ab
Adam Santiaji <adam.wikrama@gmail.com>
2019-07-17 10:46:29 +0700

    react component

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">TV Series App</h1>
+        </header>
+          <p className="App-intro">
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+      </div>
+    );
+  }
+}
+
+export default App;
